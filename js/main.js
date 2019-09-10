2c28503344b13d32cf142ae32df4ef2ba0f98d80
Your Name <n_fainer@fanshaweonline.ca>
2019-09-10 12:03:07 -0400

    latest update

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import Person from  "./modules/DataObject.js";
+
+// IIFE Immediately invoked function expression
+(() => {
+    console.log('Connected')
+
+    console.log(Person);
+
+    debugger;
+})();
---
+++ js/modules/DataObject.js
@@ -0,0 +1,11 @@
+// objects are just collections of keys and values
+// remember to seperate the keys and values with a comma
+
+const Person = {
+    name: "Liam",
+    eyeColor: "blue",
+    hairColor: "black",
+    role: "Student"
+}
+
+export default Person;
