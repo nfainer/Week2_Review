58678217f782e8471ec40741aa3a47e299ea52c1
Your Name <n_fainer@fanshaweonline.ca>
2019-09-16 18:54:48 -0400

    added js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
-import Person from  "./modules/DataObject.js";
+import Team from  "./modules/DataObject.js";
 
 // IIFE Immediately invoked function expression
 (() => {
-    console.log('Connected')
+    console.log('Fired!')
 
-    console.log(Person);
 
-    debugger;
+
+
+    function showProfData() {
+
+        // change data on the page here
+        let currentProf = this.textContent;
+
+        // grab the bio container and get ready to populate its content
+        const bioInfo = document.querySelector('.bio-wrapper').children;
+
+        // this refers to the heading tag (the first child of the bio-wrapper div)
+        bioInfo[0].textContent = Team[currentProf].name;
+
+        // this refers to the paragraph tag (the second child of the bio-wrappper div)
+        bioInfo[1].textContent = Team[currentProf].role;
+
+        bioInfo[3].src = `images/${Team[currentProf].avatar}`
+
+    }
+
+
+    for (let prof in Team){
+        console.log(prof);
+
+        let teamButton = document.createElement('button');
+
+        teamButton.textContent = prof;
+        teamButton.addEventListener('click', showProfData);
+
+        document.querySelector('#section1').appendChild(teamButton);
+    };
+   
+ 
 })();
--- js/modules/DataObject.js
+++ js/modules/DataObject.js
@@ -1,11 +1,40 @@
 // objects are just collections of keys and values
 // remember to seperate the keys and values with a comma
 
-const Person = {
-    name: "Liam",
-    eyeColor: "blue",
-    hairColor: "black",
-    role: "Student"
+
+const Team = {
+    Trevor : {
+        name: 'Trevor',
+        eyecolor: 'String',
+        haircolor:  'string',
+        role: 'string',
+        avatar: 'olaf.jpeg'
+    },
+
+    Jarrod : {
+        name: 'Jarrod',
+        eyecolor: 'String',
+        haircolor:  'string',
+        role: 'string',
+        avatar: 'zoolander.jpg'
+    },
+
+    Justin : {
+        name: 'Justin',
+        eyecolor: 'String',
+        haircolor:  'string',
+        role: 'string',
+        avatar: 'justin.jpeg'
+    },
+
+    Joe : {
+        name: 'Joe',
+        eyecolor: 'String',
+        haircolor:  'string',
+        role: 'string',
+        avatar: 'pooh.jpeg'
+    },
+
 }
 
-export default Person;
+export default Team;
